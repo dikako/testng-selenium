7ffd54e2b52b58ead94975bed32f628663d547ab
Dika <fransiskus.setiawan@stockbit.com>
2025-08-24 12:27:18 +0700

    ci: enable headless Chrome for GitHub Actions and log OS in welcome message

--- src/test/java/dibimbing/core/DriverManager.java
+++ src/test/java/dibimbing/core/DriverManager.java
@@ -3,6 +3,7 @@ package dibimbing.core;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverManager {
@@ -13,7 +14,19 @@ public class DriverManager {
         switch (browser.toLowerCase()) {
             case "chrome" -> {
                 WebDriverManager.chromedriver().setup();
-                webDriver = new ChromeDriver();
+
+                String githubActions = System.getenv("GITHUB_ACTIONS");
+                if (githubActions != null && githubActions.equals("true")) {
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--headless");
+                    options.addArguments("--disable-gpu");
+                    options.addArguments("--window-size=1920,1080"); // Recommended for headless mode
+                    options.addArguments("--no-sandbox"); // Often required in CI/CD environments
+                    options.addArguments("--disable-dev-shm-usage"); // Recommended for Docker environments
+                    webDriver = new ChromeDriver(options);
+                } else {
+                    webDriver = new ChromeDriver();
+                }
             }
             case "firefox" -> {
                 WebDriverManager.firefoxdriver().setup();
